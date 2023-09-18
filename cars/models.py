b09d0e06b08ad59ba735c0b6fec5680ad8043d84
Sharvari-kasture <sharvarimk22@gmail.com>
2023-09-18 18:49:46 +0530

    Task(9):Create Car and Maker models

---
+++ cars/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Maker(models.Model):
+    name = models.CharField(max_length=255)
+    number_of_cars = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Car(models.Model):
+    maker = models.ForeignKey(Maker, on_delete=models.CASCADE)
+    car_name = models.CharField(max_length=255)
+    hpp = models.IntegerField()
+    launch_date = models.DateField()
+
+    def __str__(self):
+        return self.car_name
+
