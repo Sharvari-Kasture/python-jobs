cc1c3bb646cd252b8faa95d4db8e765e88c0e7fa
Sharvari-kasture <sharvarimk22@gmail.com>
2023-09-20 23:18:44 +0530

    Task(22):Implement basic authentication in the app and make sure only authenticated users access the HomePage of the project.

--- cars/views.py
+++ cars/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from xcv_seo.mixin import SEOMixin
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render, redirect
@@ -24,6 +26,78 @@ class CustomLogoutView(LogoutView):
         context = super().get_context_data(**kwargs)
         context['custom_variable'] = 'This is a custom variable'
         return context
+    
+@login_required
+class HomeView(SEOMixin, TemplateView):
+    template_name = 'home.html'
+    extra_context = {
+        "roles": [
+            {
+                "name": "Senior Developer"
+            },
+            {
+                "name": "Developer"
+            },
+            {
+                "name": "Intern"
+            },
+            {
+                "name": "Summer Intern"
+            }
+        ],
+        "responsibilities": [
+            {
+                "name": "Develop applications"
+            },
+            {
+                "name": "Write tests"
+            },
+            {
+                "name": "Write documentation"
+            },
+            {
+                "name": "Deploy applications"
+            }
+        ],
+        "requirements": [
+            {
+                "name": "Minimum",
+                "items": [
+                    "Excellent communication skills",
+                    "Strong analytical skills",
+                    "Obsessed with details",
+                    "Programming experience",
+                    "Experience with one or more general-purpose languages",
+                    "Expertise in Python 3.7+"
+                ]
+            },
+            {
+                "name": "Preferred",
+                "items": [
+                    "Linux",
+                    "Shell scripting",
+                    "Git (GitHub and/or Bitbucket)",
+                    "Django",
+                    "Flask",
+                    "JavaScript",
+                    "HTML5/CSS",
+                    "Bootstrap",
+                ]
+            },
+            {
+                "name": "Extra",
+                "items": [
+                    "Numpy",
+                    "Pandas",
+                    "D3",
+                    "Solved Project Euler (<a href='https://projecteuler.net/'>projecteuler.net</a>) problems",
+                ]
+            }
+        ]
+    }
+
+class ApplyView(SEOMixin, TemplateView):
+    template_name = 'apply.html'
 
 def my_first_function_view(request):
     return HttpResponse("My First Function !@#")
