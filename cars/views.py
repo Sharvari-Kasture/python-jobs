cf36f653c48ca64a04b4b5b21aa51989a028a23a
Sharvari-kasture <sharvarimk22@gmail.com>
2023-09-20 18:09:34 +0530

    Task(15): In the “cars/list/” route, only display the cars with launch date in future and Car names that start with ‘F’.

--- cars/views.py
+++ cars/views.py
@@ -1,10 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView, LogoutView
-from xcv_seo.mixin import SEOMixin
 from django.shortcuts import render
 from cars.models import Car
+from django.utils import timezone
 from django.http import HttpResponse
 
 class CustomLoginView(LoginView):
@@ -31,9 +30,10 @@ def test_view(request):
     info = "This is the information returned from the test view."
     return HttpResponse(info)
 
+
 def car_list(request):
-    # Retrieve a list of all cars
-    cars = Car.objects.all()
+    # Retrieve a list of cars with names starting with 'F'
+    cars = Car.objects.filter(car_name__startswith='F')
 
     # Pass the list of cars to the template
     context = {
