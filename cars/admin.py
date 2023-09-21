69f8d271c2f527c07929b8533be69d49c696d6e3
Sharvari-kasture <sharvarimk22@gmail.com>
2023-09-21 16:21:17 +0530

    Task(37):On the django admin panel display the list of Makers along with the cars they made such that if cars’ count is less than 2 then display car Name and horsepower, if cars’ count is between 2 and 5 then display only car names and anything above that display all the cars in a dropdown.

--- cars/admin.py
+++ cars/admin.py
@@ -10,6 +10,22 @@ class CarAdmin(admin.ModelAdmin):
 
 @admin.register(Maker)
 class MakerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'number_of_cars', 'updated_at')
+    list_display = ('name', 'display_cars', 'number_of_cars', 'updated_at')
     search_fields = ('name', 'number_of_cars')
 
+    def display_cars(self, obj):
+        # Get the cars associated with the maker
+        cars = obj.car_set.all()
+        car_count = cars.count()
+
+        if car_count < 2:
+            # Display car Name and horsepower
+            return ', '.join([f"{car.car_name} ({car.hpp} HP)" for car in cars])
+        elif 2 <= car_count <= 5:
+            # Display only car names
+            return ', '.join([car.car_name for car in cars])
+        else:
+            # Display all cars in a dropdown
+            return 'All Cars (Dropdown)'
+
+    display_cars.short_description = 'Cars Made'
